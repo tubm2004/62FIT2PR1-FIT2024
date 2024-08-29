baf1e66d23b37379bd3647e1d009374a6324cb7f
tubm2004 <tuhanu2022@gmail.com>
2024-08-29 08:36:42 +0700

    Upload exercise 6 - ComputeExpression

---
+++ src/day01_introduction/demo/exercise6/ComputeExpression.java
@@ -0,0 +1,7 @@
+package day01_introduction.demo.exercise6;
+
+public class ComputeExpression {
+	public static void main(String[] args) {
+		System.out.println("The result is: " + (9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5));
+	}
+}
